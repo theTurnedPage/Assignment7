75bb2b1d4d09a5da4d9e5be7eccf7833a1c49d04
Page <158675598+theTurnedPage@users.noreply.github.com>
2024-11-14 09:44:02 -0600

    Making adjustments per Assignment 7 code review

--- src/com/coderscampus/assignment7/CustomArrayList.java
+++ src/com/coderscampus/assignment7/CustomArrayList.java
@@ -9,10 +9,28 @@ public class CustomArrayList<T> implements CustomList<T> {
 		@Override
 		public boolean add(T item) {
 			
-			ensureCapacity(); //method to "back" the Object array
-			items[size++] = item; //insert element into Object array, increment the size variable
+			ensureCapacity();
+			items[size++] = item;
 			return true;
 		}
+		
+		@Override
+		public boolean add(int index, T item) throws IndexOutOfBoundsException {
+			
+			ensureCapacity(); 
+			
+			if (index < 0 || index > size) {
+	            throw new IndexOutOfBoundsException("Invalid index: " + index);
+	        } else {
+	        	
+	            for (int i = size; i > index; i--) {
+	            	items[i] = items[i - 1];            	
+	            }
+	            items[index] = item;
+	        }               
+			
+			return true;	
+		}		
 
 		@Override
 		public int getSize() {		
@@ -27,59 +45,6 @@ public class CustomArrayList<T> implements CustomList<T> {
 			return (T) items[index];
 		}
 			
-		private void ensureCapacity() { //renamed per suggestion from Assignment 5 code review
-			
-			if (size == items.length) {
-				//double size of array
-				items = Arrays.copyOf(items, items.length * 2); //using this per assignment 5 code review
-				//items = backArraySize(items);
-				System.out.println("\n***Increase size of array***\n" + "\n New Size: " + items.length + "\n");
-			} else {
-				System.out.println("No need to increase size of Array yet");
-			}
-			
-		}
-		//just in case I'm not allowed to use java.util.arrays, here is a separate method to handle the doubling of the array
-//		private Object[] backArraySize(Object[] someArray) {
-//			
-//			//create new collection with double the size as the last
-//			Object[] doubledArray = new Object[someArray.length * 2];
-//			//copy elements from previous collection to new collection
-//			System.out.println(doubledArray.length);
-//			
-//			//catch the null value at the start, use it to populate the list initially
-//			if (doubledArray[0] == null) {
-//				for (int i = 0; i < someArray.length; i++) {
-//					doubledArray[i] = someArray[i];
-//				}
-//			} else { //once initial nulls are changed above, continue to populate
-//				for (int i = someArray.length-1; i < doubledArray.length; i++) {
-//					doubledArray[i] = someArray[i];
-//				}
-//			}
-//				
-//			//send newly copied collection			
-//			return doubledArray;				
-//		}
-
-		@Override
-		public boolean add(int index, T item) throws IndexOutOfBoundsException {
-			
-			ensureCapacity(); 
-			
-			if (index < 0 || index > size) {
-	            throw new IndexOutOfBoundsException("Invalid index: " + index);
-	        } else {
-	        	
-	            for (int i = size; i > index; i--) {
-	            	items[i] = items[i - 1];            	
-	            }
-	            items[index] = item;//after moving other elements, insert
-	        }               
-			
-			return true;	
-		}
-
 		@Override
 		public T remove(int index) throws IndexOutOfBoundsException {
 			
@@ -90,7 +55,6 @@ public class CustomArrayList<T> implements CustomList<T> {
 	            for (int i = index; i < size - 1; i++) {
 	            	items[i] = items[i + 1];            	
 	            }
-	            //remove last element
 	            items[size - 1] = null;
 	            size--;
 	        }               							
@@ -98,4 +62,11 @@ public class CustomArrayList<T> implements CustomList<T> {
 			return (T) items[index];
 		}
 		
+		private void ensureCapacity() { 
+			
+			if (size == items.length) {				
+				items = Arrays.copyOf(items, items.length * 2);							
+			}
+			
+		}				
 	}
--- test/com/coderscampus/assignment7/CustomArrayListTest.java
+++ test/com/coderscampus/assignment7/CustomArrayListTest.java
@@ -25,7 +25,6 @@ class CustomArrayListTest<T> {
 		
 		//Arrange
 		CustomArrayListTest<Integer> sut = new CustomArrayListTest<>();
-		//using public instead
 		
 		//Act  
 		for (int i = 0; i < 40; i++) {
@@ -33,15 +32,43 @@ class CustomArrayListTest<T> {
 		}		
 									
  		sut.should_move_items_towards_right_upon_add(5, 99);
+ 		int removedItem = sut.should_remove_item_from_list_then_push_items_left(5);
 		
 		
 		System.out.println("Number at Index 5:" + sut.should_get_item_at_index_from_list(5));
-		sut.should_remove_item_from_list_then_push_items_left(1);
+		System.out.println("Removed item: " + removedItem);
 		int actualResult = sut.should_get_size_of_list();
 				
 		//Assert		
-		assertEquals(39, actualResult);		
+		assertEquals(39, actualResult);
+		assertEquals(99, removedItem);
 	}
+	
+	@Test
+	boolean should_add_items_to_list(T item) {					
+		
+		should_start_back_size();
+		items[size++] = item;					
+		return true;
+	}
+	
+	@Test
+	boolean should_move_items_towards_right_upon_add(int index, T item) throws IndexOutOfBoundsException{
+			
+		should_start_back_size();
+		
+		if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        } else {
+        	
+            for (int i = size; i > index; i--) {
+            	items[i] = items[i - 1];            	
+            }
+            items[index] = item;
+        }               
+		
+		return true;			
+	}	
 	
 	@Test
 	public int should_get_size_of_list() {		
@@ -54,38 +81,11 @@ class CustomArrayListTest<T> {
 			throw new IndexOutOfBoundsException("Index Out of Bounds Exception!");
 		}
 		return (T) items[index];
-	}	
-	
-	@Test
-	boolean should_add_items_to_list(T item) {					
-		
-		should_start_back_size();
-		items[size++] = item; //insert element into Object array, increment the size variable					
-		return true;
-	}
-	
-	@Test
-	boolean should_move_items_towards_right_upon_add(int index, T item) throws IndexOutOfBoundsException{
-			
-		 //increase size of array to avoid IndexOutOfBoundsException if possible
-		should_start_back_size();
-		
-		if (index < 0 || index > size) {
-            throw new IndexOutOfBoundsException("Invalid index: " + index);
-        } else {
-        	
-            for (int i = size; i > index; i--) {
-            	items[i] = items[i - 1];            	
-            }
-            items[index] = item;//after moving other elements, insert
-        }               
-		
-		return true;			
-	}
+	}		
 	
 	@Test
 	private T should_remove_item_from_list_then_push_items_left(int index) throws IndexOutOfBoundsException {
-		
+		T removedItem = (T) items[index];
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Invalid index: " + index);
         } else {
@@ -93,44 +93,18 @@ class CustomArrayListTest<T> {
             for (int i = index; i < size - 1; i++) {
             	items[i] = items[i + 1];            	
             }
-            //remove last element
             items[size - 1] = null;
             size--;
         }               							
 		
-		return (T) items[index];
-	}		
-	
-//	@Test
-////	private Object[] should_back_size_of_array(Object[] someArray) {
-////		
-////		//create new collection with double the size as the last
-////		Object[] doubledArray = new Object[someArray.length * 2];
-////		//copy elements from previous collection to new collection
-////		System.out.println(doubledArray.length);
-////		
-////		//catch the null value at the start, use it to populate the list initially
-////		if (doubledArray[0] == null) {
-////			for (int i = 0; i < someArray.length; i++) {
-////				doubledArray[i] = someArray[i];
-////			}
-////		} else { //once initial nulls are changed above, continue to populate
-////			for (int i = someArray.length-1; i < doubledArray.length; i++) {
-////				doubledArray[i] = someArray[i];
-////			}
-////		}
-////			
-////		//send newly copied collection			
-////		return doubledArray;				
-////	}
+		return (T) removedItem;
+	}			
 	
 	@Test
 	private void should_start_back_size() {
 		
 		if (size == items.length) {
-			//double size of array
-			items = Arrays.copyOf(items, items.length * 2); //Using this instead per assignment 5 review
-			//items = should_back_size_of_array(items);
+			items = Arrays.copyOf(items, items.length * 2);
 			System.out.println("\n***Increase size of array***\n" + "\n New Size: " + items.length + "\n");
 		} else {
 			System.out.println("No need to increase size of Array yet");
